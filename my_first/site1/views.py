c42743e8d74e9b56ad27f8efba461e655ee31d3f
AleksandrBelak <s_belyak@mail.ru>
2023-06-25 09:04:51 +0300

    Initial commit

---
+++ my_first/site1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('<h1>Главная</h1>')
+
+def about(request):
+    return HttpResponse('<h2>о нас</h2>')
